e7c4d7d0521b27c41a979592a3ff011748ef7752
RunsWithFire <RunsWithFire@gmail.com>
2015-08-21 09:46:18 -0400

    SETUP IS HARD ;-;

---
+++ webapp/js/InitializeParse.js
@@ -0,0 +1,1 @@
+Parse.initialize("InANTLFid5husUCuYc4gbaq97qUTV8uNDwsxhrUb", "Dd0ZivjCzyQe7rOjtRFXSU78WpQvxt0nnLeuWKwF");
---
+++ webapp/js/main.js
@@ -0,0 +1,5 @@
+var app = angular.module('membershipApp', []);
+
+app.controller('membershipController', function($scope) {
+
+});
